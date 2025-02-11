bf5fcde07f66bdfec00308518bc267a36a38ae5f
laharigp <lahari.p@s.amity.edu>
2025-02-11 16:04:52 +0530

    my first commit of AUB

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int num1, num2, sum;
+	printf("enter thr first number: ");
+	scanf("%d", &num1);
+	printf("enter the second number: ");
+	scanf("%d",&num2);
+	sum = num1 + num2;
+	print("the sum of %d and %d is: %d\n", num1, num2, sum);
+	return 0;
+}
