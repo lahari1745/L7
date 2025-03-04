b38feebe1298238323bd510226a12d80f2f25ba5
lahari <lahari.p@s.amity.edu>
2025-03-04 11:17:03 +0530

    c prgograming

---
+++ c3.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+	int n;
+	printf("enter the postive integer:");
+	scanf("%d",&n);
+       	printf("even numbers from 1ton%d:\n"n);
+	for(int i=2; i<=n; i+=2)
+{
+	printf("%d",i);
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
